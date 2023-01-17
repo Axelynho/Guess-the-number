c62a61686aef46ad9a84b3b9771bd8b5f6217847
Axelynho <honplus45@mail.ru>
2023-01-17 22:01:54 +0400

    final version

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ input.focus();
 function handleSubmit(event) {
     event.preventDefault();
 
-    console.log(input.value);
+    processInput(input.value);
 
     input.value = '';
 }
@@ -26,4 +26,40 @@ function printMessage(message) {
     li.textContent = message;
 
     output.appendChild(li);
+}
+
+function clearOutput() {
+    for (let i = 0; i < output.children.length; i++) {
+        output.removeChild(output.children[i]);
+    }
+}
+
+function processInput(input) {
+    if (!input) return;
+
+    if (!name) {
+        name = input;
+        clearOutput();
+        printMessage(`${name}, загадано число от 0 до 100. 
+        Попробуй отгадать его за наименьшее количество попыток. 
+        После каждой попытки я скажу "Less than that", "Greater than that" или "Got it!".`);
+        return;
+    }
+
+    printMessage(input);
+
+    let guess = Number.parseInt(input);
+    if(Number.isNaN(guess)) return;
+    guesses +=1;
+    if(guess > number) {
+        printMessage('Less than that,try again')
+    } else if (guess < number) {
+        printMessage('Greater than that,try again')
+    } else {
+        printMessage(`Got it! ${guess}.`);
+        printMessage(`You won in ${guesses} tries`);
+        printMessage('Game over');
+
+        prompt.remove();
+    }
 }
