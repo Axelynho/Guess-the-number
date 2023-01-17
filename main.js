78a2f298fcd71eda93a8fc8391b562d3acec8df2
Axelynho <honplus45@mail.ru>
2023-01-17 20:33:59 +0400

    let / const

---
+++ main.js
@@ -0,0 +1,7 @@
+let name = '';
+let number = Math.floor(Math.random() * 100);
+let guesses = 0;
+
+const output = document.querySelector('#output');
+const prompt = document.querySelector('#prompt');
+const input = document.querySelector('#prompt input');
