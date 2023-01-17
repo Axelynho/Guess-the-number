8a11ffdd5befbfd0b80562cdd7a489ac364d3208
Axelynho <honplus45@mail.ru>
2023-01-17 20:37:35 +0400

    input

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ let guesses = 0;
 const output = document.querySelector('#output');
 const prompt = document.querySelector('#prompt');
 const input = document.querySelector('#prompt input');
+
+prompt.addEventListener('submit', handleSubmit);
+
+input.focus();
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(input.value);
+
+    input.value = '';
+}
